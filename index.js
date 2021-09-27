f83c93e8bfb3b281b991afbd1d9cbcc9a284f8a6
byteOwle <ikenarut@yahoo.com>
2021-09-27 11:28:29 -0400

    This is the code from weather here as of video 5-4

---
+++ index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import Datastore from "nedb";
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
+// console.log(process.env);
+const app = express();
+
+app.listen(5500, () => {
+  console.log("listening at 5500");
+});
+
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if(err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    })
+})
+
+
+app.post('/api', (request, response) => {
+    console.log('I got a request!');
+    console.log(request.body);
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+
+    database.insert(data);
+    response.json(data);
+})
+
+
+app.get('/weather/:latlon', async (request, response) => {
+    console.log(request.params)
+    const latlon = request.params.latlon.split(',');
+    const lat = latlon[0];
+    const lon = latlon[1];
+    console.log(lat, lon);
+    const owm_api_key = process.env.API_KEY;
+    
+    const weather_url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${owm_api_key}&units=metric`;
+    const weather_response = await fetch(weather_url);
+    const weather_data = await weather_response.json();
+
+    const aq_url = `https://docs.openaq.org/v2/latest?coordinates=${lat},${lon}`;
+    const aq_response = await fetch(aq_url);
+    const aq_data = await aq_response.json();
+
+    const data = {
+        weather: weather_data,
+        air_quality: aq_data
+    };
+
+    response.json(data);
+});
